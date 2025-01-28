d8dc96e703e4ea9e2d50d46d88311a94388481a8
MichaelJohansyah <166629794+MichaelJohansyah@users.noreply.github.com>
2025-01-28 07:04:58 +0700

    tambah penjelasan

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,3 +1,10 @@
+'''
+    intinya : 
+    Langsung sorting list dulu, terus bandingkan nilai dari elemen di
+    index i dengan index di sebelahnya, bisa sebelah kiri (i - 1), atau 
+    sebelah kanan (i + 1). Kalau nilainya sama, berarti ada nilai duplikat.
+'''
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         nums.sort()
